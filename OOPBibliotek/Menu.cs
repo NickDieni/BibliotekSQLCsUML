477866100b78e012da62067a9c6729b69ada4db0
NickDieni <nickevereye@gmail.com>
2023-04-20 08:56:50 +0200

    Add project files.

---
+++ OOPBibliotek/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBibliotek
+{
+    internal class Menu
+    {
+        public void Pickmenu()
+        {
+            Bibliotek nyBib = new Bibliotek();
+            Bog nyBog = new Bog();
+            Forfatter nyFor = new Forfatter();
+
+            Console.WriteLine("v-- Pick --v");
+            Console.WriteLine("");
+            Console.WriteLine("1 to add books");
+            Console.WriteLine("2 to add authors");
+            Console.WriteLine("3 for library");
+
+            string? Checks = Console.ReadLine();
+
+            switch (Checks)
+            {
+                case "1":
+                    Console.Clear();
+                    nyBog
+                    break;
+                case "2":
+                    Console.Clear();
+                    nyFor
+                    break;
+                case "3":
+                    nyBib
+                    break;
+                default:
+                    Console.WriteLine("Fejl prøv igen");
+                    Console.ReadKey();
+                    Pickmenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ OOPBibliotek/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPBibliotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu nyMenu = new Menu();
+            nyMenu.Pickmenu();
+        }
+    }
+}
